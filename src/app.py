8494e4a3dbcdb860e90994195637e5f42a2ae8ed
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2024-12-09 16:29:21 +0100

    made some small changes

--- src/app.py
+++ src/app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pygwalker as pyg
 # Define the navigation menu
 pg = st.navigation([
-    st.Page("pages/analysis.py", title="Analysis", icon="🥳"),
     st.Page("pages/intro.py", title="Intro", icon="🥲"),
-    st.Page("pages/references.py", title=" References", icon="🥳"),
-    st.Page("pages/impact.py", title ="Umpact", icon ="🥳")
+    st.Page("pages/analysis.py", title="Analysis", icon="🥳"),
+    st.Page("pages/impact.py", title ="Umpact", icon ="🥳"),
+    st.Page("pages/references.py", title=" References", icon="🥳")
 ])
 
 # Run the navigation system
