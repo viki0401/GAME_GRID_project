e8b796d1d76d1a859489def88201c1662b4d0212
Viktoriia Rein <viktoriiarein@49cf29fc-c2b2-4404-8421-89bf3bf1aa0d.fritz.box>
2024-12-18 18:50:32 +0100

    adds filled pages qith graphs and text

--- src/app.py
+++ src/app.py
@@ -1,16 +1,11 @@
 import streamlit as st
-import pygwalker as pyg
 # Define the navigation menu
 pg = st.navigation([
-    st.Page("pages/intro.py", title="Intro", icon="🥲"),
-    st.Page("pages/analysis.py", title="Analysis", icon="🥳"),
-    st.Page("pages/impact.py", title ="Impact", icon ="🥳"),
-    st.Page("pages/financial.py", title="Financial", icon="🥲"),
-    st.Page("pages/twitch_e1.py", title ="Twitch_e1", icon ="🥳"),
-    st.Page("pages/genres.py", title ="Genres", icon ="🥳"),
-    st.Page("pages/genre.py", title ="Genre", icon ="🥳"),
-    st.Page("pages/platform.py", title ="Platform", icon ="🥳"),
-    st.Page("pages/references.py", title=" References", icon="🥳")
+    st.Page("pages/intro.py", title="Problem Statement", icon="🔸", default=True),
+      st.Page("pages/what.py", title="What?", icon="🗣️"),
+    st.Page("pages/twitch.py", title ="How?", icon ="🔸"),
+     st.Page("pages/timing.py",title ="When?", icon ="🔸"),
+     st.Page("pages/recomendation.py",title ="Key Takeaways", icon ="🔑")
 ])
 
 # Run the navigation system
